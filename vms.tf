c6e7943d12035ac4f1b51639cc6a5ef123487b53
Your Name <alexander.klashitsky@gmail.com>
2022-06-17 00:38:12 +0300

    working state

--- vms.tf
+++ vms.tf
@@ -1,12 +1,78 @@
 
-# scale set
+# # scale set
+# data "azurerm_shared_image_version" "VMimage" {
+#   name                = "1.0.0"
+#   image_name          = "app"
+#   gallery_name        = "gallry"
+#   resource_group_name = var.resource_group_name
+
+# }
+
+resource "azurerm_virtual_machine_scale_set" "scaleSet" {
+  name                = "vmscaleset"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = "Standard_DS1_v2"
+    tier     = "Standard"
+    capacity = 1
+  }
+
+  storage_profile_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun           = 0
+    caching       = "ReadWrite"
+    create_option = "Empty"
+    disk_size_gb  = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "vmlab"
+    admin_username       = "app"
+    admin_password       = var.secret
+    # custom_data          = file("web.conf")
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPConfiguration"
+      subnet_id                              = azurerm_subnet.public.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.backendPool.id]
+      primary                                = true
+    }
+  }
+
+  # tags = var.tags
+}
 # resource "azurerm_virtual_machine_scale_set" "scaleSet" {
 #   name                = "vmscaleset"
 #   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   # upgrade_policy_mode = "Manual"
+#   resource_group_name = azurerm_resource_group.rg.name
 
-#     # automatic rolling upgrade
+
+#   # automatic rolling upgrade
 #   automatic_os_upgrade = true
 #   upgrade_policy_mode  = "Rolling"
 
@@ -17,8 +83,8 @@
 #     pause_time_between_batches              = "PT0S"
 #   }
 
-# health_probe_id = azurerm_lb_probe.LB.id
-  
+#   health_probe_id = azurerm_lb_probe.LB.id
+
 
 #   sku {
 #     name     = "Standard_DS1_v2"
@@ -31,14 +97,20 @@
 #     offer     = "UbuntuServer"
 #     sku       = "16.04-LTS"
 #     version   = "latest"
+#     # source_image_id = data.azurerm_shared_image.VMimage.id
+
 #   }
+
 
 #   storage_profile_os_disk {
 #     name              = ""
 #     caching           = "ReadWrite"
 #     create_option     = "FromImage"
 #     managed_disk_type = "Standard_LRS"
+
+
 #   }
+
 
 #   storage_profile_data_disk {
 #     lun           = 0
@@ -51,11 +123,14 @@
 #     computer_name_prefix = "vmlab"
 #     admin_username       = "app"
 #     admin_password       = var.secret
-#     # custom_data          = file("web.conf")
+#     #  custom_data          = file("web.conf")
+
+
 #   }
 
 #   os_profile_linux_config {
 #     disable_password_authentication = false
+
 #   }
 
 #   network_profile {
@@ -69,108 +144,109 @@
 #       primary                                = true
 #     }
 #   }
-
 #   #  tags = var.tags
-# }
-
-
-# resource "azurerm_monitor_autoscale_setting" "AutoscaleSetting" {
-#   name                = "myAutoscaleSetting"
-#   resource_group_name = var.resource_group_name
-#   location            = var.location
-#   target_resource_id  = azurerm_virtual_machine_scale_set.scaleSet.id
-
-#   profile {
-#     name = "defaultProfile"
-
-#     capacity {
-#       default = 1
-#       minimum = 1
-#       maximum = 3
-#     }
-
-#     rule {
-#       metric_trigger {
-#         metric_name        = "Percentage CPU"
-#         metric_resource_id = azurerm_virtual_machine_scale_set.scaleSet.id
-#         time_grain         = "PT1M"
-#         statistic          = "Average"
-#         time_window        = "PT5M"
-#         time_aggregation   = "Average"
-#         operator           = "GreaterThan"
-#         threshold          = 75
-#         metric_namespace   = "microsoft.compute/virtualmachinescalesets"
-#         dimensions {
-#           name     = "AppName"
-#           operator = "Equals"
-#           values   = ["App1"]
-#         }
-#       }
-
-#       scale_action {
-#         direction = "Increase"
-#         type      = "ChangeCount"
-#         value     = "1"
-#         cooldown  = "PT1M"
-#       }
-#     }
-
-#     rule {
-#       metric_trigger {
-#         metric_name        = "Percentage CPU"
-#         metric_resource_id = azurerm_virtual_machine_scale_set.scaleSet.id
-#         time_grain         = "PT1M"
-#         statistic          = "Average"
-#         time_window        = "PT5M"
-#         time_aggregation   = "Average"
-#         operator           = "LessThan"
-#         threshold          = 25
-#       }
-
-#       scale_action {
-#         direction = "Decrease"
-#         type      = "ChangeCount"
-#         value     = "1"
-#         cooldown  = "PT1M"
-#       }
-#     }
-#   }
-
-  
-# }
-
-# # Create virtual machine terminal
-# resource "azurerm_linux_virtual_machine" "terminal" {
-#   name                  = "terminal"
-#   location              = var.location
-#   resource_group_name   = var.resource_group_name
-#   network_interface_ids = [azurerm_network_interface.appNic.id]
-#   size                  = "Standard_F2"
-#   os_disk {
-#     name                 = "myOsDisk"
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
-
-
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "UbuntuServer"
-#     sku       = "18.04-LTS"
-#     version   = "latest"
-#   }
-
-#   computer_name                   = "terminal"
-#   admin_username                  = "app"
-#   admin_password                  = var.secret
-#   disable_password_authentication = false
 
 # }
+# elastic scale
 
+resource "azurerm_monitor_autoscale_setting" "AutoscaleSetting" {
+  name                = "myAutoscaleSetting"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  target_resource_id  = azurerm_virtual_machine_scale_set.scaleSet.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 3
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.scaleSet.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
+        dimensions {
+          name     = "AppName"
+          operator = "Equals"
+          values   = ["App1"]
+        }
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.scaleSet.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+
+
+}
+
+# Create virtual machine terminal
+resource "azurerm_linux_virtual_machine" "terminal" {
+  name                  = "terminal"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.appNic.id]
+  size                  = "Standard_F2"
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name                   = "terminal"
+  admin_username                  = "app"
+  admin_password                  = var.secret
+  disable_password_authentication = false
+
+}
+
+# Mannaged DB service
 
 resource "azurerm_postgresql_flexible_server" "psqlservice" {
   name                   = "psqlservice"
-  resource_group_name    = var.resource_group_name
+  resource_group_name    = azurerm_resource_group.rg.name
   location               = var.location
   version                = "13" # "12"
   delegated_subnet_id    = azurerm_subnet.privete.id
@@ -224,7 +300,7 @@ resource "azurerm_postgresql_flexible_server_configuration" "flexible_server_con
 
 # resource "azurerm_postgresql_flexible_server" "SQL" {
 #   name                   = "psqlserviceflexibleserver"
-#   resource_group_name    = var.resource_group_name
+#   resource_group_name    = azurerm_resource_group.rg.name
 #   location               = var.location
 #   version                = "12"
 #   delegated_subnet_id    = azurerm_subnet.privete.id
@@ -243,7 +319,7 @@ resource "azurerm_postgresql_flexible_server_configuration" "flexible_server_con
 # resource "azurerm_linux_virtual_machine" "appserver4" {
 #   name                  = "AppServer4n"
 #   location              = var.location
-#   resource_group_name   = var.resource_group_name
+#   resource_group_name   = azurerm_resource_group.rg.name
 #   network_interface_ids = [azurerm_network_interface.appNic2.id]
 #   size                  = "Standard_F2"
 #   os_disk {
@@ -269,7 +345,7 @@ resource "azurerm_postgresql_flexible_server_configuration" "flexible_server_con
 # resource "azurerm_linux_virtual_machine" "appserver3" {
 #   name                  = "AppServer3"
 #   location              = var.location
-#   resource_group_name   = var.resource_group_name
+#   resource_group_name   = azurerm_resource_group.rg.name
 #   network_interface_ids = [azurerm_network_interface.appNic3.id]
 #   size                  = "Standard_F2"
 #   os_disk {
@@ -296,7 +372,7 @@ resource "azurerm_postgresql_flexible_server_configuration" "flexible_server_con
 # # resource "azurerm_linux_virtual_machine" "dbserver" {
 # #   name                  = "db"
 # #   location              = var.location
-# #   resource_group_name   = var.resource_group_name
+# #   resource_group_name   = azurerm_resource_group.rg.name
 # #   network_interface_ids = [azurerm_network_interface.dbNic.id]
 # #   size                  = "Standard_DS1_v2"
 # #   os_disk {
@@ -324,7 +400,7 @@ resource "azurerm_postgresql_flexible_server_configuration" "flexible_server_con
 # resource "azurerm_virtual_machine_scale_set" "scaleSet" {
 #   name                = "scaleSet"
 #   location            = var.location
-#   resource_group_name = var.resource_group_name
+#   resource_group_name = azurerm_resource_group.rg.name
 #   upgrade_policy_mode = "Manual"
 
 #   sku {
@@ -388,7 +464,7 @@ resource "azurerm_postgresql_flexible_server_configuration" "flexible_server_con
 # resource "azurerm_lb" "vmss" {
 #   name                = "vmss-lb"
 #   location            = var.location
-#   resource_group_name = var.resource_group_name
+#   resource_group_name = azurerm_resource_group.rg.name
 
 
 #   frontend_ip_configuration {
@@ -413,7 +489,7 @@ resource "azurerm_postgresql_flexible_server_configuration" "flexible_server_con
 # }
 
 # resource "azurerm_lb_rule" "lbnatrule" {
-#   #   resource_group_name = var.resource_group_name
+#   #   resource_group_name = azurerm_resource_group.rg.name
 #   loadbalancer_id = azurerm_lb.vmss.id
 #   name            = "http"
 #   protocol        = "Tcp"
